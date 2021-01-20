a658e0cc8ff14afc3b9e1e9929e2f3450c81e5be
Danijel Stanic <danijel.stanich@gmail.com>
2021-01-20 15:39:47 +0100

    Added calculator's screen display functionality

--- scripts/app.js
+++ scripts/app.js
@@ -25,3 +25,24 @@ function operate (operator, x , y) {
         return divide(x,y);
     }
 }
+
+function keyPress(e) {
+    if (e.target.className === 'key num-key') {
+        displayValue += e.target.value;
+        screen.innerText = displayValue;
+    } else if (e.target.id === "key-C") {
+       displayValue = "";
+       screen.innerText = displayValue;
+
+    } else if (e.target.id === 'key-del') {
+        displayValue = displayValue.slice(0, -1)
+        screen.innerText = displayValue;
+    }
+    
+}
+
+let screen = document.querySelector('#screen')
+let displayValue = "";
+
+let keys = document.querySelectorAll(".key");
+keys.forEach(key => key.addEventListener('click', keyPress))
